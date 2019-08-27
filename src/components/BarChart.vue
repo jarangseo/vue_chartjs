0aa1ac2e59497ec464f586e160f8a559ad362d14
서자랑 <seojarang@seojalang-ui-MacBookPro.local>
2019-08-28 07:53:50 +0900

    let's use chart.js

---
+++ src/components/BarChart.vue
@@ -0,0 +1,32 @@
+<template>
+    <canvas id="barChart" ref="barChart" width="400" height="400"></canvas>
+</template>
+<script>
+
+export default {
+    props: ['propsdata'],
+    mounted() {
+    // var ctx = document.getElementById('barChart').getContext('2d');
+    var that = this;
+    var barChart = new this.$_Chart(this.$refs.barChart.getContext('2d'), {
+      type: 'bar',
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: this.propsdata
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          },
+          onClick: function() {
+              that.$emit('refresh:chart')
+          }
+      }
+    });
+  }
+}
+</script>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,27 @@
+<template>
+    <canvas id="lineChart" ref="lineChart" width="400" height="400"></canvas>
+</template>
+<script>
+export default {
+    props: ['propsdata'],
+    mounted() {
+        // var ctx = this.$refs.lineChart
+        var lineChart = new this.$_Chart(this.$refs.lineChart, {
+            type: 'line',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: this.propsdata
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+}
+</script>
